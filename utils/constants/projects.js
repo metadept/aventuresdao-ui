ee0a403f9b6843dcf19c996998854c0049fd7f54
beowulf518 <thmsshanahan0815@gmail.com>
2022-02-21 15:30:22 +0000

    [feat] implement new projects

--- utils/constants/projects.js
+++ utils/constants/projects.js
@@ -24,6 +24,41 @@ const PROJECTS = [
     icon: '/assets/images/projects/herosNFT.jpg',
     description: ''
   },
+  {
+    id: 'ember-finance',
+    icon: '/assets/images/projects/ember-finance.jpg',
+    description: ''
+  },
+  {
+    id: 'pollen',
+    icon: '/assets/images/projects/pollen.jpg',
+    description: ''
+  },
+  {
+    id: 'heroes-chained',
+    icon: '/assets/images/projects/heroes-chained.jpg',
+    description: ''
+  },
+  {
+    id: 'dexalot',
+    icon: '/assets/images/projects/dexalot.jpg',
+    description: ''
+  },
+  {
+    id: 'shade',
+    icon: '/assets/images/projects/shade.jpg',
+    description: ''
+  },
+  {
+    id: 'alter',
+    icon: '/assets/images/projects/alter.jpg',
+    description: ''
+  },
+  {
+    id: 'fief',
+    icon: '/assets/images/projects/fief.jpg',
+    description: ''
+  },
 ]
 
 export default PROJECTS
